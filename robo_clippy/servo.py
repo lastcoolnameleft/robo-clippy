b469106ccdbf936d64893d2b0440456e51f30fc3
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-11-20 23:27:04 -0600

    Add servo test

--- robo_clippy/servo.py
+++ robo_clippy/servo.py
@@ -12,6 +12,18 @@ MOUTH_SERVO = 0
 RIGHT_SERVO = 1
 LEFT_SERVO = 2
 
+MOUTH_NEUTRAL = 490
+MOUTH_HALF = 430
+MOUTH_FULL = 340
+
+LEFT_UP = 430
+LEFT_MIDDLE = 400
+LEFT_DOWN = 350
+
+RIGHT_UP = 370
+RIGHT_MIDDLE = 335
+RIGHT_DOWN = 285
+
 class Servo(object):
 
     # Initialise the PCA9685 using the default address (0x40).
@@ -62,41 +74,37 @@ class Servo(object):
         print('Clippy speak')
         if self.mouth_position == 0:
             print('Clippy move mouth half forward')
-#            self.left_eye_middle()
-#            self.right_eye_middle()
             self.mouth_forward_half()
             self.mouth_position = 1
         else:
             print('Clippy move mouth full forward')
-#            self.left_eye_middle()
-#            self.right_eye_middle()
             self.mouth_forward_full()
             self.mouth_position = 0
 
     # Everything below here should be "private"-ish
     def mouth_neutral(self):
-        self.pwm.set_pwm(MOUTH_SERVO, 0, 430)
+        self.pwm.set_pwm(MOUTH_SERVO, 0, MOUTH_NEUTRAL)
 
     def mouth_forward_full(self):
-        self.pwm.set_pwm(MOUTH_SERVO, 0, 240)
+        self.pwm.set_pwm(MOUTH_SERVO, 0, MOUTH_FULL)
 
     def mouth_forward_half(self):
-        self.pwm.set_pwm(MOUTH_SERVO, 0, 320)
+        self.pwm.set_pwm(MOUTH_SERVO, 0, MOUTH_HALF)
 
     def left_eye_up(self):
-        self.pwm.set_pwm(LEFT_SERVO, 0, 395)
+        self.pwm.set_pwm(LEFT_SERVO, 0, LEFT_UP)
 
     def left_eye_down(self):
-        self.pwm.set_pwm(LEFT_SERVO, 0, 290)
+        self.pwm.set_pwm(LEFT_SERVO, 0, LEFT_DOWN)
 
     def left_eye_middle(self):
-        self.pwm.set_pwm(LEFT_SERVO, 0, 360)
+        self.pwm.set_pwm(LEFT_SERVO, 0, LEFT_MIDDLE)
 
     def right_eye_up(self):
-        self.pwm.set_pwm(RIGHT_SERVO, 0, 380)
+        self.pwm.set_pwm(RIGHT_SERVO, 0, RIGHT_UP)
 
     def right_eye_down(self):
-        self.pwm.set_pwm(RIGHT_SERVO, 0, 300)
+        self.pwm.set_pwm(RIGHT_SERVO, 0, RIGHT_DOWN)
 
     def right_eye_middle(self):
-        self.pwm.set_pwm(RIGHT_SERVO, 0, 350)
+        self.pwm.set_pwm(RIGHT_SERVO, 0, RIGHT_MIDDLE)
---
+++ servo-calibrate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from robo_clippy import servo
+s = servo.Servo()
+
+print('MOUTH_NEUTRAL=' + str(servo.MOUTH_NEUTRAL))
+s.pwm.set_pwm(servo.MOUTH_SERVO, 0, servo.MOUTH_NEUTRAL)
+print('MOUTH_HALF=' + str(servo.MOUTH_HALF))
+s.pwm.set_pwm(servo.MOUTH_SERVO, 0, servo.MOUTH_HALF)
+print('MOUTH_FULL=' + str(servo.MOUTH_FULL))
+s.pwm.set_pwm(servo.MOUTH_SERVO, 0, servo.MOUTH_FULL)
+
+print('LEFT_UP=' + str(servo.LEFT_UP))
+s.pwm.set_pwm(servo.LEFT_SERVO, 0, servo.LEFT_UP)
+print('LEFT_MIDDLE=' + str(servo.LEFT_MIDDLE))
+s.pwm.set_pwm(servo.LEFT_SERVO, 0, servo.LEFT_MIDDLE)
+print('LEFT_DOWN=' + str(servo.LEFT_DOWN))
+s.pwm.set_pwm(servo.LEFT_SERVO, 0, servo.LEFT_DOWN)
+
+print('RIGHT_UP=' + str(servo.RIGHT_UP))
+s.pwm.set_pwm(servo.RIGHT_SERVO, 0, servo.RIGHT_UP)
+print('RIGHT_MIDDLE=' + str(servo.RIGHT_MIDDLE))
+s.pwm.set_pwm(servo.LEFT_SERVO, 0, servo.RIGHT_MIDDLE)
+print('RIGHT_DOWN=' + str(servo.RIGHT_DOWN))
+s.pwm.set_pwm(servo.LEFT_SERVO, 0, servo.RIGHT_DOWN)
